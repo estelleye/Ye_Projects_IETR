059f53480a37dff7d6ae195cf62c2eb3af8c6d40
yetian <ye.tian@insa-rennes.fr>
2020-07-06 07:47:22 +0200

    PS & PL share by BRAM

---
+++ PS_PL_SharebyBRAM/PS_PL_SharebyBRAM.sdk/test/Debug/src/helloworld.d
@@ -0,0 +1,41 @@
+src/helloworld.o src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h ../../test_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../test_bsp/ps7_cortexa9_0/include/sleep.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../test_bsp/ps7_cortexa9_0/include/sleep.h:
---
+++ PS_PL_SharebyBRAM/readme.txt
@@ -0,0 +1,8 @@
+The purpose is to use Block Memory to perform data interaction or data sharing between PS and PL. 
+
+Write data to BRAM through the Master GP0 ??port of zynq PS, and then read the data through Mater GP1 on PS side, 
+and print the result to serial terminal display. 
+
+It involves the use of IPs such as AXI BRAM Controller and Block Memery Generator.
+
+The  development environment version: Vivado 2015.4 Xilinx SDK 2015.4.
